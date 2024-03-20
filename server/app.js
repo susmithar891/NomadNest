f71c6f22295735a89c8ea45a78d1cf26a324d604
arramanila <161479664+arramanila@users.noreply.github.com>
2024-03-20 20:18:48 +0530

    backend logic for register is done

--- server/app.js
+++ server/app.js
@@ -1,18 +1,53 @@
 const express = require("express")
 require("dotenv").config()
+const path = require('path')
 const session = require("express-session")
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 require('./conn')
+const user = require('./models/user.model')
 
 
 
 const app = express()
+app.use(express.json())
 port = process.env.PORT || 3000
 
 
 
 
+app.get('/',(req,res)=>{
+    res.status(200).sendFile(path.join(__dirname,'models','hotels.json'))
+})
+
+
+app.post("/api/sign-up/",async(req,res)=>{
+
+    const checkaval_email = await user.findOne({email : req.body.email});
+
+    if(checkaval_email){
+        res.status(409).json("Email already exists");
+    }
+    else{
+        try{
+            const salt = await bcrypt.genSalt();
+            const hashedPass = await bcrypt.hash(req.body.password,salt);
+
+            const newUser = await new user({firstName : req.body.firstname,lastName : req.body.lastname , email : req.body.email,password : hashedPass});
+            await newUser.save();
+            res.sendStatus(200)
+            
+        }
+        catch(err){
+            res.status(400).send(err);
+        }
+        
+    }  
+    
+})
+
+
+
 app.listen(port,()=>{
-    console.log('server started at port 9000')
+    console.log(`server started at port ${port}`)
 })
