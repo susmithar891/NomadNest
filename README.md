dea913ba939fa565e1ef5c24ee43d58db6275f2f
Monishareddy14 <161188713+Monishareddy14@users.noreply.github.com>
2024-03-20 22:15:06 +0530

    backend logic for login is made

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # NomadNest
+
+After cloning the repo
+1.run npm install
+2.in .env file paste MONGO_URL = mongodb://0.0.0.0:27017/Nomadnest
--- server/app.js
+++ server/app.js
@@ -8,46 +8,77 @@ require('./conn')
 const user = require('./models/user.model')
 
 
-
 const app = express()
-app.use(express.json())
 port = process.env.PORT || 3000
 
 
 
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors({
+    origin: "*"
+}))
 
-app.get('/',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,'models','hotels.json'))
+
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'models', 'hotels.json'))
 })
 
 
-app.post("/api/sign-up/",async(req,res)=>{
+app.post("/api/sign-up", async (req, res) => {
 
-    const checkaval_email = await user.findOne({email : req.body.email});
+    const checkaval_email = await user.findOne({ email: req.body.email });
 
-    if(checkaval_email){
+    if (checkaval_email) {
         res.status(409).json("Email already exists");
     }
-    else{
-        try{
+    else {
+        try {
             const salt = await bcrypt.genSalt();
-            const hashedPass = await bcrypt.hash(req.body.password,salt);
+            const hashedPass = await bcrypt.hash(req.body.password, salt);
 
-            const newUser = await new user({firstName : req.body.firstname,lastName : req.body.lastname , email : req.body.email,password : hashedPass});
+            const newUser = await new user({ firstName: req.body.firstname, lastName: req.body.lastname, email: req.body.email, password: hashedPass });
             await newUser.save();
             res.sendStatus(200)
-            
+
         }
-        catch(err){
+        catch (err) {
             res.status(400).send(err);
         }
-        
-    }  
-    
+
+    }
+
+})
+
+
+app.post("/api/sign-in", async (req, res) => {    
+
+    const loggeduser = await user.findOne({email : req.body.email})
+    if (loggeduser) {
+        await bcrypt.compare(req.body.password, loggeduser.password, (err, resp) => {
+            if (err) {
+                return res.sendStatus(400)
+            }
+            if (resp) {
+                return res.sendStatus(200)
+
+            }
+            else {
+                return res.sendStatus(401)
+            }
+        })
+
+    }
+    else {
+        return res.sendStatus(400)
+    }
+
 })
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server started at port ${port}`)
 })
