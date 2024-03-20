1f9fcb6fa6d2ae09401baea4060d6d5b35e379b8
anurag90000 <152231706+anurag90000@users.noreply.github.com>
2024-03-21 04:47:08 +0530

    get all the data of hotels from the database to render  them

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Navbar } from './Navbar'
 import Roomcard from './Roomcard'
 import hotels from '../media/input.json'
+import request from '../api/axios'
 
 
 
 export const Homepage = () => {
+
+    const [data, setData] = useState([]);
+    const [user, setUser] = useState({});
+    const [location, setLocation] = useState("");
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await request.post('/data/?location=', {
+                    headers: { 'Content-Type': 'application/json' },
+                });
+                console.log(res);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
+
+
     return (
         <>
             <Navbar />
-            
+
             <div className=' my-3 w-75 mx-auto'>
                 <h2 className='px-4 w-75 my-3'>
                     Search for rooms.....
@@ -25,10 +50,10 @@ export const Homepage = () => {
                         </select>
                     </div>
                     <div className="w-50 m-1" id="prefill">
-                        <input id="startDate" className="form-control p-3" type="date" required/>
+                        <input id="startDate" className="form-control p-3" type="date" required />
                     </div>
                     <div className="w-50 m-1" id="prefill">
-                        <input id="endDate" className="form-control p-3" type="date" placeholder='check-out' required/>
+                        <input id="endDate" className="form-control p-3" type="date" placeholder='check-out' required />
                     </div>
                     <div className="w-50 my-auto p-2 border border-secondary-subtle rounded-1">
                         <ul className="list-unstyled  my-auto p-2 profile-menu">
@@ -51,7 +76,7 @@ export const Homepage = () => {
                                             </div>
                                             <div className='d-flex w-50 mx-2 my-1 justify-content'>
                                                 <button className='rounded mx-1' style={{ background: '#66ccff' }}>-</button>
-                                                <input type='text' className='form-control' required/>
+                                                <input type='text' className='form-control' required />
                                                 <button className='rounded mx-1' style={{ background: '#66ccff' }}>+</button>
                                             </div>
                                         </div>
@@ -61,7 +86,7 @@ export const Homepage = () => {
                                             </div>
                                             <div className='d-flex w-50 mx-2 my-1 justify-content'>
                                                 <button className='rounded mx-1' style={{ background: '#66ccff' }}>-</button>
-                                                <input type='text' className='form-control' required/>
+                                                <input type='text' className='form-control' required />
                                                 <button className='rounded mx-1' style={{ background: '#66ccff' }}>+</button>
                                             </div>
                                         </div>
@@ -71,7 +96,7 @@ export const Homepage = () => {
                                             </div>
                                             <div className='d-flex w-50 mx-2 my-1 justify-content'>
                                                 <button className='rounded mx-1' style={{ background: '#66ccff' }}>-</button>
-                                                <input type='text' className='form-control' required/>
+                                                <input type='text' className='form-control' required />
                                                 <button className='rounded mx-1' style={{ background: '#66ccff' }}>+</button>
                                             </div>
                                         </div>
@@ -96,12 +121,12 @@ export const Homepage = () => {
 
                 {hotels.map(function (element) {
                     return (
-                        <Link className='m-3' to = {'/home/'+element.hotelId} key={element.hotelId} style={{ textDecoration: 'none' }}>
-                            <Roomcard hotelId={element.hotelId} hotelName={element.hotelname} hotelimg={'hotel'+element.hotelId+'.jpeg'} hotelpre={element.preview}/>
+                        <Link className='m-3' to={'/home/' + element.hotelId} key={element.hotelId} style={{ textDecoration: 'none' }}>
+                            <Roomcard hotelId={element.hotelId} hotelName={element.hotelname} hotelimg={'hotel' + element.hotelId + '.jpeg'} hotelpre={element.preview} />
                         </Link>
                     );
                 })}
-                               
+
 
 
             </div>
--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const bcrypt = require('bcrypt')
 require('./conn')
 const user = require('./models/user.model')
+const hotels = require('./hotels.json')
 
 
 const app = express()
@@ -60,6 +61,38 @@ const redirectLogin = (req,res,next) => {
 
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, 'models', 'hotels.json'))
+})
+
+
+app.post('/data',async(req,res) => {
+    const loc = req.query.location;
+    var data;
+    if(loc === ""){
+        data = hotels
+    }
+    else{
+        data = hotels.map((ele)=>{
+            return ele.location === loc
+        })
+    }
+    
+    try{
+        const user_det = await user.findOne({id : req.session.userId}).select('-password -updatedAt -email -createdAt -__v -_id')
+        const qunt = {
+            username : user_det,
+            data : data
+        }
+    
+        res.status(200).send(qunt)
+    }
+    catch(err){
+        // res.status(400).send(err)
+        throw err
+    }
+    
+
+
+    
 })
 
 
