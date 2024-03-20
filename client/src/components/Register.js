b3342dca88c802ff944d649d2be56bf9465c8c64
bhavanakum <161515810+bhavanakum@users.noreply.github.com>
2024-03-21 02:00:35 +0530

    made API call from front end upon loging in
    fell into a bug ,with cors, not even sorted even after setting up access control  allow origin policy

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,134 +1,182 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+
+
 
 
 const Register = () => {
-  return (
-    <section className="text-center ">
-      <div
-        className="p-5 bg-image"
-        style={{
-          background: "#3a447a",
-          color:"white",
-          font:"Simplifica",
-          height: 300
-        }}
-      >
-        <Link className="mx-auto my-0 text-uppercase h1 text-decoration-none" to="/">Nomad Nest</Link>
-      </div>
-      <div
-        className="card mx-4 mx-md-5 shadow-5-strong"
-        style={{
-          marginTop: "-100px",
-          background: "hsla(0, 0%, 100%, 0.8)",
-          backdropFilter: "blur(30px)"
-        }}
-      >
-        <div className="card-body py-5 px-md-5">
+	const [firstName, setFirst] = useState("")
+	const [lastName, setLast] = useState("")
+	const [email, setEmail] = useState("")
+	const [pass, setPass] = useState("")
+	const [remeb, setRememb] = useState(false)
 
-          <div className="row d-flex justify-content-center">
+	const handleSubmit = async(e) => {
+		e.preventDefault()
+		console.log("firstname : ",firstName)
+		console.log("lastname : ",lastName)
+		console.log("email : ",email)
+		console.log("password : ",pass)
+		console.log("remember me : ",remeb)
 
-            <div className="col-lg-8">
-              <h2 className="fw-bold mb-5">Sign up</h2>
-              <form>
-                <div className="row">
-                  <div className="col-md-6 mb-4">
-                    <div className="form-outline">
-                      <input
-                        type="text"
-                        id="form3Example1"
-                        className="form-control"
-                        required
-                      />
-                      <label className="form-label" htmlFor="form3Example1">
-                        First name
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-md-6 mb-4">
-                    <div className="form-outline">
-                      <input
-                        type="text"
-                        id="form3Example2"
-                        className="form-control"
-                        required
-                      />
-                      <label className="form-label" htmlFor="form3Example2">
-                        Last name
-                      </label>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-outline mb-4">
-                  <input type="email" id="form3Example3" className="form-control" required/>
-                  <label className="form-label" htmlFor="form3Example3">
-                    Email address
-                  </label>
-                </div>
-                <div className="form-outline mb-4">
-                  <input
-                    type="password"
-                    id="form3Example4"
-                    className="form-control"
-                    required
-                  />
-                  <label className="form-label" htmlFor="form3Example4">
-                    Password
-                  </label>
-                </div>
-                <div className="form-check d-flex justify-content-center mb-4">
-                  <input
-                    className="form-check-input me-2"
-                    type="checkbox"
-                    defaultValue=""
-                    id="form2Example33"
-                    defaultChecked=""
-                  />
-                  <label className="form-check-label" htmlFor="form2Example33">
-                    Remember me
-                  </label>
-                </div>
-                <button type="submit" className="btn btn-primary btn-block mb-4">
-                  Sign Up
-                </button>
-              </form>
+		try{
+			const res = await axios.post('https://localhost:4000/api/sign-up',
+			JSON.stringify({"firstname" : firstName,"lastname" : lastName,"email" : email,"password" : pass,"remember" : remeb}),
+			{
+				headers:{"Accept":"application/json, text/plain, /","Content-Type": "multipart/form-data"},
+				withCredientials :true
+			})
 
-                <div className="form-check d-flex justify-content-center mb-4">
-                  <div className=''>
-                    <span className='m-2'>
-                      Already have an account ? 
-                    </span>
-                    <Link to='/sign-in'>Sign In</Link>
-                  </div>
-                </div>
+			console.log(res.data)
+		}catch(err){
+			// if(!err?.response){
+			// 	setErrMsg('No Server Response')
+			// }
+			// else if(err.response?.status === 409){
+			// 	setErrMsg('username taken')
+			// }
+			// else{
+			// 	setErrMSg('Registration failed')
+			// }
+			console.log(err)
+		}
+
+	
+	}
 
 
+	return (
+		<section className="text-center ">
+			<div
+				className="p-5 bg-image"
+				style={{
+					background: "#3a447a",
+					color: "white",
+					font: "Simplifica",
+					height: 300
+				}}
+			>
+				<Link className="mx-auto my-0 text-uppercase h1 text-decoration-none" to="/">Nomad Nest</Link>
+			</div>
+			<div
+				className="card mx-4 mx-md-5 shadow-5-strong"
+				style={{
+					marginTop: "-100px",
+					background: "hsla(0, 0%, 100%, 0.8)",
+					backdropFilter: "blur(30px)"
+				}}
+			>
+				<div className="card-body py-5 px-md-5">
+
+					<div className="row d-flex justify-content-center">
+
+						<div className="col-lg-8">
+							<h2 className="fw-bold mb-5">Sign up</h2>
+							<form onSubmit={handleSubmit}>
+								<div className="row">
+									<div className="col-md-6 mb-4">
+										<div className="form-outline">
+											<input
+												type="text"
+												id="firstname"
+												className="form-control"
+												required
+												onChange={(e) => { setFirst(e.target.value) }}
+											/>
+											<label className="form-label" htmlFor="firstname">
+												First name
+											</label>
+										</div>
+									</div>
+									<div className="col-md-6 mb-4">
+										<div className="form-outline">
+											<input
+												type="text"
+												id="lastname"
+												className="form-control"
+												onChange={(e) => { setLast(e.target.value) }}
+												required
+											/>
+											<label className="form-label" htmlFor="lastname">
+												Last name
+											</label>
+										</div>
+									</div>
+								</div>
+								<div className="form-outline mb-4">
+									<input type="email" id="email" className="form-control" required onChange={(e) => { setEmail(e.target.value) }} />
+									<label className="form-label" htmlFor="email">
+										Email address
+									</label>
+								</div>
+								<div className="form-outline mb-4">
+									<input
+										type="password"
+										id="password"
+										className="form-control"
+										onChange={(e) => { setPass(e.target.value) }}
+										required
+									/>
+									<label className="form-label" htmlFor="password">
+										Password
+									</label>
+								</div>
+								<div className="form-check d-flex justify-content-center mb-4">
+									<input
+										className="form-check-input me-2"
+										type="checkbox"
+										defaultValue=""
+										id="remember"
+										defaultChecked=""
+										onChange={() => { setRememb(prevCheck => !prevCheck) }}
+									/>
+									<label className="form-check-label" htmlFor="remember">
+										Remember me
+									</label>
+								</div>
+								<button type="submit" className="btn btn-primary btn-block mb-4" >
+									Sign Up
+								</button>
+							</form>
+
+							<div className="form-check d-flex justify-content-center mb-4">
+								<div className=''>
+									<span className='m-2'>
+										Already have an account ?
+									</span>
+									<Link to='/sign-in'>Sign In</Link>
+								</div>
+							</div>
 
 
 
-                <div className="text-center">
-                  <p>or sign up with:</p>
-                  <button type="button" className="btn btn-link btn-floating mx-1">
-                    <i className="fab fa-facebook-f" />
-                  </button>
-                  <button type="button" className="btn btn-link btn-floating mx-1">
-                    <i className="fab fa-google" />
-                  </button>
-                  <button type="button" className="btn btn-link btn-floating mx-1">
-                    <i className="fab fa-twitter" />
-                  </button>
-                  <button type="button" className="btn btn-link btn-floating mx-1">
-                    <i className="fab fa-github" />
-                  </button>
-                </div>
-              
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
 
-  )
+
+							<div className="text-center">
+								<p>or sign up with:</p>
+								<button type="button" className="btn btn-link btn-floating mx-1">
+									<i className="fab fa-facebook-f" />
+								</button>
+								<button type="button" className="btn btn-link btn-floating mx-1">
+									<i className="fab fa-google" />
+								</button>
+								<button type="button" className="btn btn-link btn-floating mx-1">
+									<i className="fab fa-twitter" />
+								</button>
+								<button type="button" className="btn btn-link btn-floating mx-1">
+									<i className="fab fa-github" />
+								</button>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	)
 }
 
 export default Register
--- server/app.js
+++ server/app.js
@@ -7,47 +7,51 @@ const bcrypt = require('bcrypt')
 require('./conn')
 const user = require('./models/user.model')
 
-
-
+port = process.env.PORT || 4000
 const app = express()
+
+
+
+
 app.use(express.json())
-port = process.env.PORT || 3000
+app.use(express.urlencoded({extended:false}))
 
 
 
 
-app.get('/',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,'models','hotels.json'))
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'models', 'hotels.json'))
 })
 
 
-app.post("/api/sign-up/",async(req,res)=>{
+app.post("/api/sign-up/", async (req, res) => {
 
-    const checkaval_email = await user.findOne({email : req.body.email});
+    const checkaval_email = await user.findOne({ email: req.body.email });
 
-    if(checkaval_email){
+    if (checkaval_email) {
         res.status(409).json("Email already exists");
     }
-    else{
-        try{
+    else {
+        try {
             const salt = await bcrypt.genSalt();
-            const hashedPass = await bcrypt.hash(req.body.password,salt);
+            const hashedPass = await bcrypt.hash(req.body.password, salt);
 
-            const newUser = await new user({firstName : req.body.firstname,lastName : req.body.lastname , email : req.body.email,password : hashedPass});
+            const newUser = await new user({ firstName: req.body.firstname, lastName: req.body.lastname, email: req.body.email, password: hashedPass });
             await newUser.save();
+
             res.sendStatus(200)
-            
+
         }
-        catch(err){
+        catch (err) {
             res.status(400).send(err);
         }
-        
-    }  
-    
+
+    }
+
 })
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server started at port ${port}`)
 })
