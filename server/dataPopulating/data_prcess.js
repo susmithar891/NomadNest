9fbb349fbc08a70564d3ddd5a857bdd81cce36b0
Mounika1809 <161490807+Mounika1809@users.noreply.github.com>
2024-04-01 06:27:13 +0530

    created data and uploaded into database

---
+++ server/dataPopulating/data_prcess.js
@@ -0,0 +1,26 @@
+const hotels = require('./Nomadnest.hotels.json');
+const roomtypes = require('./roomType_in.json');
+const fs = require('fs');
+
+const roomTypes = [];
+
+let count = 0;
+for (let i = 0; i < hotels.length; i++) {
+    for (let j = 0; j < roomtypes.length; j++) {
+        let temp = { ...roomtypes[j] };
+        temp.hotelId = hotels[i]._id.$oid;
+        count += 1;
+        roomTypes.push(temp);
+        
+    }
+}
+
+const jsonData = JSON.stringify(roomTypes, null, 2); // Convert roomTypes array to JSON string
+
+fs.writeFile('roomType_fin.json', jsonData, (err) => {
+    if (err) {
+        console.error('Error writing to file:', err);
+    } else {
+        console.log('Data written to file successfully.');
+    }
+});
