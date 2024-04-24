d083217700378b53e6f29bf91e19781606a67f32
anurag90000 <152231706+anurag90000@users.noreply.github.com>
2024-04-24 18:20:53 +0530

    resolved bug with price of each room in the populated data

--- server/dataPopulating/data_rooms.js
+++ server/dataPopulating/data_rooms.js
@@ -13,28 +13,31 @@ console.log(hotels.length)
 console.log(roomtypes.length)
 console.log(rooms.length)
 let count  = 0
-for(let i=0;i<hotels.length;i++){
+// for(let i=0;i<hotels.length;i++){
     for(let j=0;j<roomtypes.length;j++){
-        for(let k=0;k<rooms.length;k++){
-            let temp = { ...rooms[k]}
-            temp.hotelId = hotels[i]._id.$oid
-            temp.hotelName = hotels[i].hotelName
+        for(let k=0;k<10;k++){
+            let temp = {}
+            temp.reservedDates = []
+            temp.hotelId = roomtypes[j].hotelId
+            temp.hotelName = roomtypes[j].hotelName
             temp.roomType = roomtypes[j].roomType
             temp.price = roomtypes[j].price
+            
             let randomNumber = Math.floor(Math.random() * (500 - 100 + 1)) + 100;
             while(randomNumber in room_num){
                 randomNumber = Math.floor(Math.random() * (500 - 100 + 1)) + 100;
             }
             room_num.push(randomNumber)
             temp.roomNo = randomNumber
-            delete temp.preview
+            // delete temp.preview
             Rooms.push(temp)
             count+=1;
-            console.log(count)
+            // console.log(count)
+            console.log(k," : " ,roomtypes[j].roomType," " ,roomtypes[j].price)
         }
 
     }
-}
+// }
 
 
 const jsonData = JSON.stringify(Rooms, null, 2); // Convert roomTypes array to JSON string
