65257f1e6c4cbda91f511197b50ef42e73c0fee2
Monishareddy14 <161188713+Monishareddy14@users.noreply.github.com>
2024-05-02 15:47:14 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -194,7 +194,8 @@ app.post("/api/sign-in", redirectHome, async (req, res) => {
 
 app.post('/api/logout', (req, res) => {
     // if(req.cookies && req.cookies.session_token){
-    res.clearCookie('session_token',{path:'/',domain:'nomad-api-dot-centered-oasis-418917.wn.r.appspot.com',httpOnly: true,sameSite : "none",secure : true});
+    res.clearCookie('session_token',{path:'/',domain:'nomad-api-dot-centered-oasis-418917.wn.r.appspot.com',httpOnly:true});
+    // res.clearCookie('session_token')
     // }
     res.end()
 })
