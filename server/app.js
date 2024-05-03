5542b3925c15e9f5c05778483f64bef0d0d53aef
Monishareddy14 <161188713+Monishareddy14@users.noreply.github.com>
2024-05-03 17:42:29 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
 
 const corsOption = {
     origin: process.env.CLIENT_URL,
-    // credentials: true,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }
 
