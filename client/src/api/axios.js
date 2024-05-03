0e72fd7fb01387b3f8dafc71626574bfc4ffc905
Monishareddy14 <161188713+Monishareddy14@users.noreply.github.com>
2024-05-03 16:44:54 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const request = axios.create({
-  baseURL: "https://nomad-api-dot-centered-oasis-418917.wn.r.appspot.com/",
+  baseURL: "https://nomad-api-dot-project2-422210.wn.r.appspot.com",
   // baseURL : 'http://localhost:4000/',
   timeout: 20000,
   withCredentials: true,
